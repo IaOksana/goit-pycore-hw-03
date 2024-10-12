4b6cc81cf8f5229de99ed0380d854b87ada33edb
IaOksana <iashchuk.oksana@gmail.com>
2024-10-12 16:11:22 +0400

    Виконано

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 
-def get_days_from_today():
-    # Якась дата
-    another_date = datetime(input('Введіть дату у форматі: '))
-
+def get_days_from_today(another_date_str : str):
+    # Перетворення рядка у дату
+    try :
+        another_date = datetime.strptime(another_date_str, '%Y-%m-%d')
+    except ValueError :
+        if input("Неправильний формат даних. Натисніть '+' якщо хочете спробувати ще") == "+" :
+            get_days_from_today(input("Введіть дату у форматі 'РРРР-ММ-ДД': ")) 
+        return
+    
     # Поточна дата
     current_date = datetime.now()
 
     # Розрахунок кількості днів
-    days_since = current_date.toordinal() - another_date.toordinal()
+    days_since = (current_date - another_date).days
 
-    print(days_since)
+    print(f"Різниця у днях: {days_since}")
 
-
-get_days_from_today()
+# Запуск функції з введенням користувача
+get_days_from_today(input("Введіть дату у форматі 'РРРР-ММ-ДД': "))
